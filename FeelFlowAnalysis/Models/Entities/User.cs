d4524500f199660ac80077d0e189d5de05ddd4ce
troubleShooter239 <morgun2282@gmail.com>
2024-03-20 20:08:33 +0300

    Add user properties

--- FeelFlowAnalysis/Models/Entities/User.cs
+++ FeelFlowAnalysis/Models/Entities/User.cs
@@ -31,6 +31,16 @@ public class User
     public CreditCard CardDetails { get; set; }
 
     // Summary:
+    //     Gets or sets the subscription start date.
+    [BsonElement("subscription_start_date")]
+    public DateTime SubscriptionStartDate { get; set; } = DateTime.Now;
+    
+    // Summary:
+    //     Gets or sets the history of the user prompts.
+    [BsonElement("history")]
+    public List<Dictionary<string, dynamic>> History { get; set; } = [];
+
+    // Summary:
     //     Gets or sets the role of the user.
     [BsonElement("role")]
     public UserRoles Role { get; set; } = UserRoles.WithoutSubscription;
