de3a7c2ac7b1ea1feee9cf3dd3a8bca6154fde93
troubleShooter239 <morgun2282@gmail.com>
2024-03-30 09:19:49 +0300

    Some fixes

--- FeelFlowAnalysis/Models/DTO/Analyze/AnalyzeDTO.cs
+++ FeelFlowAnalysis/Models/DTO/Analyze/AnalyzeDTO.cs
@@ -6,23 +6,23 @@ namespace FeelFlowAnalysis.Models.DTO.Analyze;
 public class AnalyzeDTO : IEnumerable<KeyValuePair<string, object>>
 {
     [JsonPropertyName("region")]
-    public required Region Region { get; set; }
+    public Region Region { get; set; }
     [JsonPropertyName("face_confidence")]
     public float FaceConfidence { get; set; }
     [JsonPropertyName("age")]
     public int Age { get; set; }
     [JsonPropertyName("emotion")]
-    public required Emotion Emotion { get; set; }
+    public Emotion Emotion { get; set; }
     [JsonPropertyName("dominant_emotion")]
-    public required string DominantEmotion { get; set; }
+    public string DominantEmotion { get; set; }
     [JsonPropertyName("gender")]
-    public required Gender Gender { get; set; }
+    public Gender Gender { get; set; }
     [JsonPropertyName("dominant_gender")]
-    public required string DominantGender { get; set; }
+    public string DominantGender { get; set; }
     [JsonPropertyName("race")]
-    public required Race Race { get; set; }
+    public Race Race { get; set; }
     [JsonPropertyName("dominant_race")]
-    public required string DominantRace { get; set; }
+    public string DominantRace { get; set; }
 
     public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
     {
