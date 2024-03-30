f426f8a143afb0de48ecabdd4b4338ede9f250e5
mnogaBukav <morgun2282@gmail.com>
2024-03-30 20:32:36 +0300

    HTML/CSS fixes

--- FeelFlowAnalysis/Models/DTO/Analyze/AnalyzeDTOList.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FeelFlowAnalysis.Models.DTO.Analyze;
-
-public class AnalyzeDTOList
-{
-    public List<AnalyzeDTO>? Items { get; set; }
-}
--- FeelFlowAnalysis/Models/DTO/Verify/VerifyDTO.cs
+++ FeelFlowAnalysis/Models/DTO/Verify/VerifyDTO.cs
@@ -1,11 +1,27 @@
-﻿namespace FeelFlowAnalysis.Models.DTO.Verify;
+﻿using System.Text.Json.Serialization;
+
+namespace FeelFlowAnalysis.Models.DTO.Verify;
 
 public class VerifyDTO
 {
+    [JsonPropertyName("verified")]
     public bool Verified { get; set; }
+    [JsonPropertyName("distance")]
     public float Distance { get; set; }
+    [JsonPropertyName("threshold")]
     public float Threshold { get; set; }
+    [JsonPropertyName("model")]
     public string? Model { get; set; }
+    [JsonPropertyName("simularity_metric")]
     public string? SimilarityMetric { get; set; }
-    public Dictionary<string, Region>? FacialAreas { get; set; }
+    [JsonPropertyName("facial_areas")]
+    public FacialAreas? FacialAreas { get; set; }
+}
+
+public class FacialAreas
+{
+    [JsonPropertyName("img1")]
+    public Region? Img1 { get; set; }
+    [JsonPropertyName("img2")]
+    public Region? Img2 { get; set; }
 }
